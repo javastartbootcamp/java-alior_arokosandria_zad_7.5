729644f233b37dcdf480654a48ed4367e1194399
arokosandria <kmsa@interia.pl>
2023-11-06 20:17:25 +0100

    solution correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(System.in);
         Main main = new Main();
+
         main.run(scanner);
 
     }
 
-    void run(Scanner scanner){
+    void run(Scanner scanner) throws FileNotFoundException{
         scanner = new Scanner(System.in);
         System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje: ");
         String country = scanner.nextLine();
@@ -21,10 +23,22 @@ public class Main {
         Files files=new Files();
         try {
             Map<String, Country> countryMap = files.readFile(file);
+            Country country1 = null;
+            for (Map.Entry<String, Country> entry : countryMap.entrySet()) {
+                if (country.equals(entry.getKey())) {
+                    country1 = entry.getValue();
+                    System.out.println(country1.getCountryName() + " " + "(" + country1.getCode() + ")" + " ma " + country1.getAmountPeople() + " ludności");
+                    return;
+                }
             }
-        catch (FileNotFoundException e){
-            System.out.println("Brak pliku " + file + ".");
+            if (country1 == null) {
+                System.out.println("Kod kraju " + country + " nie został znaleziony");
+            }
+        }catch (IOException e) {
+            throw new FileNotFoundException("Brak pliku " + file + ".");
         }
+
+
     }
 }
 
