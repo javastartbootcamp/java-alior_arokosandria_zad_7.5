0fc6e20d66e81a6d6ba6d5a5abb471e6c62f2d1a
arokosandria <kmsa@interia.pl>
2023-11-06 19:28:21 +0100

    solution correct

--- src/main/java/Files.java
+++ src/main/java/Files.java
@@ -1,27 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 public class Files {
     public Map<String, Country> readFile() throws FileNotFoundException {
         Map<String, Country> countryMap = new HashMap<>();
         File file = new File("countries.csv");
-        if (checkFile(file)) {
-            try (Scanner scanner = new Scanner(file)) {
-                while (scanner.hasNextLine()) {
-                    String[] elements = scanner.nextLine().split(";");
-                    countryMap.put(elements[0], new Country(elements[0], elements[1], Integer.parseInt(elements[2])));
-                }
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String[] elements = scanner.nextLine().split(";");
+                countryMap.put(elements[0], new Country(elements[0], elements[1], Integer.parseInt(elements[2])));
             }
+        } catch (IOException e) {
+            throw new FileNotFoundException("Brak pliku " + file.getName() + ".");
         }
-        return countryMap;
-    }
 
-    public boolean checkFile(File file) {
-        if (file.exists()) {
-            return true;
-        }
-        System.out.println("Brak pliku " + file.getName());
-        return false;
+        return countryMap;
     }
 }
